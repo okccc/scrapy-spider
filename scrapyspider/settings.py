43b48a8d8c11532c8372003cc8f2269f0f61e2f1
okccc <1573976179@qq.com>
2018-12-25 15:00:37 +0800

    update

--- scrapyspider/settings.py
+++ scrapyspider/settings.py
@@ -56,7 +56,7 @@ DEFAULT_REQUEST_HEADERS = {
 # See https://doc.scrapy.org/en/latest/topics/downloader-middleware.html
 DOWNLOADER_MIDDLEWARES = {
    # 'scrapyspider.middlewares.ScrapyspiderDownloaderMiddleware': 543,
-   'scrapyspider.middlewares.RandomUserAgent': 100,
+   # 'scrapyspider.middlewares.RandomUserAgent': 100,
    # 'scrapyspider.middlewares.RandomProxy': 200,
    # 'scrapyspider.middlewares.SeleniumMiddleware': 300,
 }
@@ -70,12 +70,12 @@ DOWNLOADER_MIDDLEWARES = {
 # Configure item pipelines
 # See https://doc.scrapy.org/en/latest/topics/item-pipeline.html
 ITEM_PIPELINES = {
-   # 'scrapyspider.pipelines.TencentspiderPipeline': 300,
+   'scrapyspider.pipelines.TencentspiderPipeline': 300,
    # 'scrapyspider.pipelines.SunwzspiderPipeline': 300,
    # 'scrapyspider.pipelines.JianshuspiderPipeline': 300,
    # 'scrapyspider.pipelines.JianshuTwistedspiderPipline': 300,
    # 'scrapyspider.pipelines.SoyoungspiderPipeline': 300,
-   'scrapyspider.pipelines.SoufangspiderPipeline': 300,
+   # 'scrapyspider.pipelines.SoufangspiderPipeline': 300,
 }
 
 # Enable and configure the AutoThrottle extension (disabled by default)
